28bfaac929df5106d3837627588d1cee92d80b8e
devansh6177 <devansh6177@gmail.com>
2023-10-09 11:23:09 -0400

    about page animation done

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,10 +3,9 @@ import Logo from "../../assets/images/logo-s.png";
 import "./index.scss";
 import { useEffect, useState } from "react";
 import AnimatedLetters from "../AnimatedLetters";
-
+import Loader from "react-loaders";
 
 const Home = () => {
-
   const [letterClass, setLetterClass] = useState("text-animate");
   const nameArray = ["e", "v", "a", "n", "s", "h"];
   const jobArray = [
@@ -31,47 +30,50 @@ const Home = () => {
     ".",
   ];
 
-  useEffect(()=>{
-    setTimeout(()=>{
-        setLetterClass('text-animate-hover')
-    },6000)
-  },[])
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 6000);
+  }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={["H", "i", ","]}
-            index={11}
-          />
-          <br />
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={["H", "i", ","]}
+              index={11}
+            />
+            <br />
 
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={["I","'", "m"]}
-            index={16}
-          />
-          <img src={Logo} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            index={22}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            index={31}
-          />
-        </h1>
-        <h2>Web App Developer / Mobile App Developer / Javascript Expert </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={["I", "'", "m"]}
+              index={16}
+            />
+            <img src={Logo} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              index={22}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              index={31}
+            />
+          </h1>
+          <h2>Web App Developer / Mobile App Developer / Javascript Expert </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
